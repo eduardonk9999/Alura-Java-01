1a719d0bd6e1b05bbc9bf3084f535485e7333d28
eduardonk9999 <thiago.novak7@gmail.com>
2024-02-01 13:29:24 -0300

    feat: condicoes

--- src/Condicional.java
+++ src/Condicional.java
@@ -1,10 +1,21 @@
 public class Condicional {
-// teste nvim dois:
-//
-//
-//
-//
-////:
+    public static void main(String[] args) {
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = false;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
 
+        if(anoDeLancamento >= 2022){
+            System.out.println("Lancamento que os clientes estao criando");
+        } else {
+            System.out.println("Filme retro que vale a pena assistir!");
+        }
+
+        if(incluidoNoPlano == true || tipoPlano.equals("plus")) {
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Deve pagar a locao");
+        }
+    }
 
 }
